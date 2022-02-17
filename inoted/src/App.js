9c96a88409b4182861040db6e10ac7e18e82086f
Palash Rambhia <palashrambhia2@gmail.com>
2022-02-17 12:39:44 +0530

    Components and Navbar Made

--- inoted/src/App.js
+++ inoted/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Home from './components/Home';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNoted !!</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route exact path="/" element={<Home/>}></Route>
+          <Route exact path="/home" element={<Home/>}></Route>
+          <Route exact path="/about" element={<About/>}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ inoted/src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+        <h2>About Hai Bro</h2>
+    </div>
+  )
+}
