c90a491f99f18abd59bba89939ea3d16f0ba5e16
Palash Rambhia <palashrambhia2@gmail.com>
2022-02-18 21:06:18 +0530

    Notes Title Fetched

--- inoted/src/components/Home.js
+++ inoted/src/components/Home.js
@@ -1,29 +1,37 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import notecontext from "../context/Notes/NoteContext"
 
 export default function Home() {
+  const cntx = useContext(notecontext);
+  const {notes, setnotes} = cntx;
+
   return (
     <div>
       <div className="container my-3">
         <h2>Add A Note :)</h2>
         <form>
-          <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
           </div>
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" />
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" />
           </div>
-          <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+          <div className="mb-3 form-check">
+            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+            <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
           </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
+          <button type="submit" className="btn btn-primary">Submit</button>
         </form>
       </div>
-
-      <h2>Your Notes</h2>
+      <div className="container my-3">
+        <h2>Your Notes</h2>
+        {notes.map((note)=>{
+            return note.title;
+        })}
+      </div>
     </div>
   )
 }
--- inoted/src/context/Notes/NoteState.js
+++ inoted/src/context/Notes/NoteState.js
@@ -4,24 +4,32 @@ import { useState } from 'react';
 
 const NoteState = (props)=>{
 
-    // const s1 = {
-    //     "name": "Palash",
-    //     "class": "2nd Year"
-    // }
+    const inotes = [
+        {
+          "_id": "620cefd0de12518b9fe440a7",
+          "user": "6208e77b518960681c418a5f",
+          "title": "My WakeUp Alarm",
+          "description": "Wake Up Early My Bird",
+          "tag": "Personel",
+          "date": "2022-02-16T12:36:32.936Z",
+          "__v": 0
+        },
+        {
+          "_id": "620cf35ffcc1f4b0542565e3",
+          "user": "6208e77b518960681c418a5f",
+          "title": "My WakeUp Call",
+          "description": "UTH JAA BSDK",
+          "tag": "Personel",
+          "date": "2022-02-16T12:51:43.156Z",
+          "__v": 0
+        }
+      ]
 
-    // const [state, setstate] = useState(s1)
-    // const update = ()=>{
-    //     setTimeout(() => {
-    //         setstate({
-    //         "name": "Kalu",
-    //         "class": "10th"
-    //         })
-    //     }, 1000);
-    // }
-
+    const [notes, setnotes] = useState(inotes)
     // Boilerplate whenever using context
+    
     return (
-        <Notecontext.Provider>
+        <Notecontext.Provider value={{notes, setnotes}}>
             {props.children}
         </Notecontext.Provider>
     )
