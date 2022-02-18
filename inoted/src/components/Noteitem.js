ff5e42d0d429e1783e0654a32ed0608efc391948
Palash Rambhia <palashrambhia2@gmail.com>
2022-02-18 22:05:12 +0530

    Added Icons

--- inoted/src/components/Noteitem.js
+++ inoted/src/components/Noteitem.js
@@ -4,10 +4,14 @@ const Noteitem = (props) => {
     const {note} = props;
   return (
     <div className="col-md-3">
-      <div class="card my-3">
-        <div class="card-body">
-            <h5 class="card-title">{note.title}</h5>
-            <p class="card-text">{note.description}</p>
+      <div className="card my-3">
+        <div className="card-body">
+            <div className="d-flex align-items-center justify-content-between">
+                <h5 className="card-title">{note.title}</h5>
+                <i className="fas fa-solid fa-trash mx-4"></i>
+                <i className="fas fa-solid fa-pen"></i>
+            </div>
+            <p className="card-text">{note.description}</p>
         </div>
       </div>
     </div>
--- inoted/src/components/Notes.js
+++ inoted/src/components/Notes.js
@@ -11,7 +11,7 @@ const Notes = ()=>{
     <div className="row my-3">
         <h2>Your Notes</h2>
         {notes.map((note)=>{
-            return <Noteitem note={note}/>;
+            return <Noteitem key={note.id} note={note}/>;
         })}
     </div>
   )
