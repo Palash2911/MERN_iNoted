a605d59f49dcf5fb8956d0a61176f0601f973b34
Palash Rambhia <palashrambhia2@gmail.com>
2022-02-23 18:34:13 +0530

    Final Push

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,7 +1,6 @@
-import React, {useContext, useEffect, useRef, useState} from 'react'
-import notecontext from "../context/Notes/NoteContext"
+import React from 'react'
 import Addnote from './Addnote'
-import { useNavigate } from 'react-router-dom'
+
 
 const Notes = (props)=>{
   const {showAlert} = props;
